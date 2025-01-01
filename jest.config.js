8b49d2f9eef33d034140c5912bf07010c0998464
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2025-01-02 00:44:45 +0100

    Update some packages

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "/dist/",
-    "/src/app/", // Can't make jest work with some modules
-  ],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    ".+\\.js$": "ts-jest",
-  },
-  transformIgnorePatterns: ["node_modules/(?!(motion-utils|ol))"],
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    "react-markdown": "<rootDir>/src/__test__/__mocks__/react-markdown.jsx",
-  },
+export const preset = "ts-jest";
+export const testEnvironment = "jsdom";
+export const testPathIgnorePatterns = [
+  "/node_modules/",
+  "/dist/",
+  "/src/app/", // Can't make jest work with some modules
+];
+export const transform = {
+  "^.+\\.tsx?$": "ts-jest",
+  ".+\\.js$": "ts-jest",
+};
+export const transformIgnorePatterns = ["node_modules/(?!(motion-utils|ol))"];
+export const moduleNameMapper = {
+  "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  "react-markdown": "<rootDir>/src/__test__/__mocks__/react-markdown.jsx",
 };
--- src/services/highlight-esql.js
+++ src/services/highlight-esql.js
@@ -1,4 +1,4 @@
-import hljs from "highlight.js/lib/core";
+import hljs from "highlight.js/lib/core.js";
 
 function highight_esql() {
   const COMMENT_MODE = hljs.COMMENT("//", "$", {
